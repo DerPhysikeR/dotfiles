ed07a27d0dbfc5e20e03adb2df6ec5670fb5c223
Paul Reiter <reiter.paul@gmail.com>
2018-08-13 22:56:47 +0200

    Add magit, wombat theme and other stuff

--- init.el
+++ init.el
@@ -7,8 +7,9 @@
 
 ; Add Melpa as the default Emacs Package repository
 ; only contains a very limited number of packages
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
 ; Activate all the packages (in particular autoloads)
 (package-initialize)
@@ -24,22 +25,31 @@
 
 (require 'evil)
 (evil-mode t)
+(require 'evil-magit)
+(load-theme 'wombat t)
 
 (require 'evil-leader)
 (global-evil-leader-mode)
-(evil-leader/set-leader ",")
+(evil-leader/set-leader " ")
 (evil-leader/set-key
   "b" 'switch-to-buffer
-"w" 'save-buffer)
+  "w" 'save-buffer)
+
+ressources
+https://emacsdojo.github.io/
+https://cestlaz.github.io/stories/emacs/
+https://www.reddit.com/r/emacs/comments/6s5470/useful_packages/
+https://github.com/noctuid/evil-guide
 
 ; learn more
 ; https://github.com/noctuid/evil-guide
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil-leader evil))))
+ '(package-selected-packages (quote (evil-magit evil-leader evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
